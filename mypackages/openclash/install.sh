b354aeac0c5963f565f92cb7e0182b923f4efa34
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-06-01 15:24:53 +0800

    1

--- mypackages/openclash/install.sh
+++ mypackages/openclash/install.sh
@@ -1,8 +1,3 @@
-opkg update
-if [ $? -ne 0 ]; then
-    echo "更新软件源列表错误，请检查路由器自身网络连接以及是否有失效的软件源。"
-    exit 1
-fi
 opkg install depends/*.ipk
 opkg install luci-app-openclash_0.46.003-beta_all.ipk || exit 1
 mv clash clash_meta clash_tun /etc/openclash/core/
--- mypackages/unblockneteasemusic/install.sh
+++ mypackages/unblockneteasemusic/install.sh
@@ -1,7 +1,1 @@
-opkg update
-if [ $? -ne 0 ]; then
-    echo "更新软件源列表错误，请检查路由器自身网络连接以及是否有失效的软件源。"
-    exit 1
-fi
-opkg install depends/*.ipk
 opkg install luci-app-unblockneteasemusic_3.2_all.ipk
